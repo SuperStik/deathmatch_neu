98444d9769234cc349a867ea3d8ce9611f1b8c3b
SuperStik <daviswood290@gmail.com>
2022-06-07 15:10:16 -0700

    Player noclip no longer requires cheats, change name

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -1,12 +1,12 @@
 AddCSLuaFile()
 include("player_class/player_deathmatch.lua")
-GM.Name = "Team Class Deathmatch"
+GM.Name = "Deathmatch"
 GM.Author = "Stik"
 GM.Email = ""
 GM.Website = ""
 GM.IsDeathmatchDerived = true
 local adminfly = CreateConVar("dm_adminnoclip", "1", 8576, "Allow admins to noclip")
-local playerfly = CreateConVar("dm_playernoclip", "0", 24960, "Allow players to noclip")
+local playerfly = CreateConVar("dm_playernoclip", "0", 8576, "Allow players to noclip")
 
 function GM:PlayerNoClip(ply, state)
 	if not IsValid(ply) then
